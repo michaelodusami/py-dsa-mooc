66ec987b3077a788a9187f47878484cb017316a7
Michael Odusami <kyrmikee@gmail.com>
2025-02-26 10:39:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,171 @@
 import sys
 import random
+
+# ********************* 6. Own DS *********************
+
+class Stack:
+    def __init__(self):
+        self.array: list = []
+    
+    def push(self, element):
+        self.array.append(element)
+    
+    def pop(self):
+        if len(self.array) == 0:
+            raise IndexError("pop from empty stack")
+        return self.array.pop()
+
+    def top(self):
+        if len(self.array) == 0:
+            raise IndexError("top from empty stack")
+        return self.array[-1]
+    
+    def __repr__(self):
+        return str(self.array)
+
+    def __len__(self):
+        return len(self.array)
+
+def useStack():
+    stack = Stack()
+    stack.push(10)
+    stack.push(20)
+    print(stack)
+    print(f"Removing Pop: {stack.pop()}")
+    print(len(stack))
+
+useStack()
+
+# ********************* 5. Sorting *********************
+# --- arr.sort() => in place sorting
+# --- sorted(arr) => returns a sorted array
+# --- arr.sort(key=abs) => sorts according to a key
+# def cmp(a, b):
+#    print("compare", a, b)
+#    return a - b
+#
+# numbers = [4, 1, 3, 2]
+# numbers.sort(key=functools.cmp_to_key(cmp))
+
+def restaurantSet(arrivals: list, departures: list):
+    # find the highest number of customers in rhe rest at the same tmime
+    # if a customer departs at the same moment another arrives you are in the rest at the same moment
+
+    # store the target customers in some array (customers are represented by the index)
+    events = []
+    for time in arrivals:
+        events.append((time, 1))
+    for time in departures:
+        events.append((time, 2))
+    print(events)
+    events.sort()
+    print(events)
+    counter = 0
+    result = 0
+    for event in events:
+        if event[1] == 1:
+            counter += 1
+        if event[1] == 2:
+            counter -= 1
+        result = max(result, counter)
+    return result
+
+    
+class Location:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other):
+        return (self.x, self.y) == (other.x, other.y)
+
+    def __lt__(self, other):
+        return (self.x, self.y) < (other.x, other.y)
+
+    def __repr__(self):
+        return str((self.x, self.y))
+
+def hashingVSorting(numbers: list):
+    return len(set(numbers))
+
+def smallestNumbers(numbers: list):
+    n = len(numbers)
+    numbers = sorted(numbers)
+    result = float('inf')
+    for i in range(1, n):
+        before = numbers[i - 1]
+        after = numbers[i]
+        result = min(result, after - before)
+    print(result)
+
+
+# ********************* 4. Hashing *********************
+# Sets: All operations (add, in, remove) is O(1) because of hashing
+# dicts: adding, acessing, and removing data is all 0(1)
+
+
+# Common Ways To Use A Dictionary 
+
+#1. Has an element occured
+def hasElementOccuredHashing():
+    # dicts
+    seen = dict()
+    for x in [1, 2, 3]:
+        seen[x] = True
+
+    # Sets
+    seen = set()
+    for x in [1, 2, 3]:
+        seen.add(x)
+
+#2. Has an element occured
+def countingOccurences():
+    count = {}
+    for x in [1, 2, 3, 3]:
+        if x not in count:
+            count[x] = 0
+        count[x] += 1
+#3. Position of occurence
+def posOfOccurence():
+    pos = {}
+    for index, element in enumerate([1, 2, 3, 4, 4]):
+        pos[index] = element
+
+# Example. how many numbers?
+
+def howManyNumbersMyAlg(numbers: list):
+    return len(set(numbers))
+
+# Example: Mode
+
+def findModeHashingMyAlg(numbers: list):
+    mostFreq = numbers[0]
+    freq = dict()
+    for number in numbers:
+        if number not in freq:
+            freq[number] = 0
+        freq[number] += 1
+        
+        if freq[number] > freq[mostFreq]:
+            mostFreq = number
+    print(mostFreq)
+
+# Example. Rounds
+
+def rounds(numbers: list):
+    # collecting numbers from smallest to largest in n rounds
+    n = len(numbers)
+    pos = {}
+    for i, x in enumerate(numbers):
+        pos[x] = i
+        
+    rounds = 1
+    for i in range(1, n):
+        if pos[i + 1] < pos[i]:
+            rounds += 1
+
+
+
 # ********************* 3. Efficient Algorithms *********************
 # an efficient algorithm has a single for-loop that goes through the input from left to right
 # the code inside the loop is efficient so that each round in the loop takes O(1) time
@@ -78,8 +244,6 @@ def bitStringOptomized(bitString: str):
         if bitString[i] == '1':
             result += zeros
     return result
-
-print(bitStringOptomized("01001011"))
 
 # Example: List Splitting
 # You are given a list containing n integers. Your task is to count how many ways one can split the list into two parts so that both parts have the same total sum of elements.
